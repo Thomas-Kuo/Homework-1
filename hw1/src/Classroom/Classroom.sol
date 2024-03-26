7c07eb78cef0c949d09f07a4d44d811c00f87881
Thomas-Kuo <thomasanimes@gmail.com>
2024-03-26 20:28:18 +0800

    Update Classroom.sol

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -2,13 +2,24 @@
 pragma solidity ^0.8.0;
 
 /* Problem 1 Interface & Contract */
+// contract StudentV1 {
+//     // Note: You can declare some state variable
+
+//     function register() external returns (uint256) {
+//         // TODO: please add your implementaiton here
+//     }
+// }
+
 contract StudentV1 {
-    // Note: You can declare some state variable
+    uint256 private studentIdCounter = 1;  // 存储学生ID的计数器
 
     function register() external returns (uint256) {
-        // TODO: please add your implementaiton here
+        uint256 studentId = studentIdCounter;  // 获取当前ID
+        studentIdCounter++;  // 增加计数器，为下一个学生准备
+        return studentId;  // 返回新注册学生的ID
     }
 }
+
 
 /* Problem 2 Interface & Contract */
 interface IClassroomV2 {
